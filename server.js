2e3b7ea395e14b9611db87b4c6bfd79722cb0a58
yanis <yanis.legrand.1@gmail.com>
2022-08-16 20:08:02 +0200

    Optimisation du parser RSS

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ const path = require('path')
 const fs = require("fs");
 const Parser = require("rss-parser");
 
-let app = express() 
+let app = express()
 let host = "127.0.0.1"
 let port = "5555"
 app.use(express.static(path.resolve(__dirname, "wwwroot")));
 
 app.listen(
-    //process.env.PORT,
-    //process.env.IP,
+    // process.env.PORT,
+    // process.env.IP,
     // local values
-    port, 
+    port,
     host,
-    ()=>{
+    () => {
         // console.log(`Server is runing on http://${process.env.IP}:${process.env.PORT}`);
         // local values
         console.log(`Server is runing on http://${host}:${port}`);
@@ -22,65 +22,39 @@ app.listen(
 );
 
 // -- RSS Parser
+// TODO : mettre les flux RSS dans un tableau, parcourire le tableau et comparer avec le fichier JSON
 
 (async function main() {
 
+    const urls = [
+        "https://www.google.fr/alerts/feeds/15619974748131115017/8380039576618842492", // Google -> PWA
+        "https://www.youtube.com/feeds/videos.xml?channel_id=UCnEHCrot2HkySxMTmDPhZyg", // youtube -> Defend Intelligence
+        "https://korben.info/feed" // korben blog
+    ];
+
+
     const parser = new Parser();
-    const feed = await parser.parseURL("https://www.google.fr/alerts/feeds/15619974748131115017/8380039576618842492"); // PWA
-    const fileName = `./wwwroot/data/${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
-    let items = [];
+    for (let i = 0; i < urls.length; i++) {
+        const feed = await parser.parseURL(urls[i]);
+        const fileName = `./wwwroot/data/${feed.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
+        let items = [];
 
-    if (fs.existsSync(fileName)) {
-        items = require(`./${fileName}`);
-    }
-
-    await Promise.all(feed.items.map(async (currentItem) => {
-        if (items.filter((item) => isEquivalent(item, currentItem)).length <= 0) {
-            items.push(currentItem);
+        if (fs.existsSync(fileName)) {
+            items = require(`./${fileName}`);
         }
 
-    }));
+        await Promise.all(feed.items.map(async (currentItem) => {
+            if (items.filter((item) => isEquivalent(item, currentItem)).length <= 0) {
+                items.push(currentItem);
+            }
 
-    fs.writeFileSync(fileName ,JSON.stringify(items));
+        }));
 
-
-//-- FLUX RSS N°2
-
-    const feed2 = await parser.parseURL("https://www.youtube.com/feeds/videos.xml?channel_id=UCnEHCrot2HkySxMTmDPhZyg"); // defend-inteligence
-    const fileName2 = `./wwwroot/data/${feed2.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
-    let items2 = [];
-
-    if (fs.existsSync(fileName2)) {
-        items2 = require(`./${fileName2}`);
+        fs.writeFileSync(fileName, JSON.stringify(items));
+        console.log(feed);
     }
+    // const feed = await parser.parseURL(urls[i]); // PWA
 
-    await Promise.all(feed2.items.map(async (currentItem2) => {
-        if (items2.filter((item2) => isEquivalent(item2, currentItem2)).length <= 0) {
-            items2.push(currentItem2);
-        }
-
-    }));
-
-    fs.writeFileSync(fileName2 ,JSON.stringify(items2));
-
-//-- FLUX RSS N°3
-
-    const feed3 = await parser.parseURL("https://korben.info/feed"); // korben blog
-    const fileName3 = `./wwwroot/data/${feed3.title.replace(/\s+/g, "-").replace(/[/\\?%*:|"<>]/g, '').toLowerCase()}.json`;
-    let items3 = [];
-
-    if (fs.existsSync(fileName3)) {
-        items3 = require(`./${fileName3}`);
-    }
-
-    await Promise.all(feed3.items.map(async (currentItem3) => {
-        if (items3.filter((item3) => isEquivalent(item3, currentItem3)).length <= 0) {
-            items3.push(currentItem3);
-        }
-
-    }));
-
-    fs.writeFileSync(fileName3 ,JSON.stringify(items3));
 })();
 
 function isEquivalent(a, b) {
